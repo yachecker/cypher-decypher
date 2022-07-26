b9a203babffeea070208b30864081996d662c9bd
yachecker <anegmanov@bk.ru>
2022-07-26 17:13:37 +0600

    Added wrong input handler

--- createCode.py
+++ createCode.py
@@ -1,28 +1,30 @@
+import codecs
 import random
 global chars
 chars = ',`qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM;\'1234567890@#$%^&*()!йцукенгшщзфывапролдяячсмитьбюЙЦУКЕНГШЩЗФЫВАПРОЛДЯЧСМИТЬБЮ'
 global whitespace
 whitespace = ''
 
+
 def randomChars(stringLength):
     global chars
     string = ''
     global whitespace
-    for i in range(0,stringLength):
-        whitespace += chars[random.randint(0,len(chars)-1)] 
+    for i in range(0, stringLength):
+        whitespace += chars[random.randint(0, len(chars)-1)]
     for i in range(0, stringLength):
         string += chars[random.randint(0, len(chars)-1)]
     return string
 
 
 symbols = {}
-import codecs
+
 
 def createCode(stringLength):
     global whitespace
     for i in range(0, len(chars)):
         symbols[chars[i]] = randomChars(stringLength)
-    with codecs.open(f'coding.txt', 'w','utf-8') as f:
+    with codecs.open(f'coding.txt', 'w', 'utf-8') as f:
         f.write(f'coding code:\n')
         for key in symbols:
             f.write(f'{key}={symbols[key]}\n')
@@ -33,9 +35,12 @@ if __name__ == '__main__':
     while True:
         length = input('Input the length of replacable symbol: ')
         try:
-            length = int(length)
-            break
+            if int(length) > 1:
+                createCode(int(length))
+                break
+            elif int(length) <= 1:
+                print("Input number which is greater than 1")
         except ValueError:
             print("Input int only")
-    createCode(int(length))
+
     print(f'Created code with symbol length: {length}')
--- main.py
+++ main.py
@@ -9,11 +9,11 @@ while True:
     try:
         if os.path.exists(os.path.dirname(os.path.realpath(__file__))+'\coding.txt'):
             print('Successfully found coding.txt. Reading cypher')
-            with codecs.open('coding.txt', 'r','utf-8') as f:
+            with codecs.open('coding.txt', 'r', 'utf-8') as f:
                 # f.read('coding.txt')
                 lines = f.readlines()
-                #print(lines)
-                length = lines[1].index('\n') -2
+                # print(lines)
+                length = lines[1].index('\n') - 2
                 for i in range(1, len(lines)):
                     encoding[lines[i][0]] = lines[i][2:length+2].strip('\n')
                 f.close()
@@ -23,13 +23,15 @@ while True:
             while True:
                 length = input('Input the length of replacable symbol: ')
                 try:
-                    length = int(length)
-                    break
-                except ValueError:
-                    print("Input int only")
-            createCode(int(length))
+                    if int(length) > 1:
+                        createCode(int(length))
+                        break
+                    elif int(length) <= 1:
+                        print("Input number which is greater than 1")
+                except Exception() as e:
+                    pass
     except Exception as e:
-        print('Failed to read file.')
+        print(e)
 
 
 testString = input('Input string to cypher: ')
@@ -37,11 +39,11 @@ testString = input('Input string to cypher: ')
 
 def cypher(string):
     workArray = list(string)
-    #print(workArray)
+    # print(workArray)
     res = []
     resString = ''
     for i in range(0, len(workArray)):
-        #print(encoding[workArray[i]])
+        # print(encoding[workArray[i]])
         res.append(encoding[workArray[i]])
     for i in range(0, len(res)):
         resString += res[i]
@@ -57,7 +59,7 @@ def decypher(cypher):
     for i in range(length, len(cypher)+length, length):
 
         if cypher[i-length:i] in encoding.values():
-            resString += keyList[valueList.index(cypher[i-length:i])] 
+            resString += keyList[valueList.index(cypher[i-length:i])]
 
     return resString
 
